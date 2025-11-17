00e48b5a4ad21276cb8b3edd0552fbe7b8405e31
unknown <orhunarpaci2@gmail.com>
2025-11-17 22:30:51 +0300

    Initial Commit

---
+++ DMA_UART_2CH/DMA_UART_2CH.srcs/sources_1/new/DMA_UART.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 17.11.2025 22:05:10
+-- Design Name: 
+-- Module Name: DMA_UART - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DMA_UART is
+--  Port ( );
+end DMA_UART;
+
+architecture Behavioral of DMA_UART is
+
+begin
+
+
+end Behavioral;
